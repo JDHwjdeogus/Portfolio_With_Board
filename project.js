01327601836f856a18d624cbf1ebfce2aface2ba
JDHwjdeogus <unohyden18@gmail.com>
2023-02-01 16:18:11 +0900

    탭메뉴 구현했음. 프로젝트 캡처본 이미지 제작할 것

--- project.js
+++ project.js
@@ -46,4 +46,27 @@
 
 
     // 프로젝트 탭 변경 
-    
+    const tabList = document.querySelectorAll('.tab_menu .projectNameList .selector .selector-item');
+    const contents = document.querySelectorAll('.tab_menu .tabMenuContent')
+    let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#item1 활성화)
+
+    for(var i = 0; i < tabList.length; i++){
+        tabList[i].querySelector('.selector-item a').addEventListener('click', function(e){
+            e.preventDefault();
+            for(var j = 0; j < tabList.length; j++){
+                // 나머지 버튼 클래스 제거
+                tabList[j].classList.remove('active');
+
+                // 나머지 컨텐츠 display:none 처리
+                contents[j].style.display = 'none';
+            }
+
+            // 버튼 관련 이벤트
+            this.parentNode.classList.add('active');
+
+            // 버튼 클릭시 컨텐츠 전환
+            activeCont = this.getAttribute('href');
+            // console.log(activeCont);
+            document.querySelector(activeCont).style.display = 'block';
+        });
+    }
